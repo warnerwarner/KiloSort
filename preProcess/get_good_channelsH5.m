8273edd0f04e63489d9d6b640e93f142a12a57d5
warnerwarner <tom.p.a.warner@gmail.com>
2021-01-12 12:20:22 +0000

    Change buffer end cond

--- preProcess/get_good_channelsH5.m
+++ preProcess/get_good_channelsH5.m
@@ -29,7 +29,7 @@ function igood = get_good_channels(ops, chanMap)
     % from a subset of batches, count threshold crossings
     while ibatch<=Nbatch
         offset = twind + NT*(ibatch-1) + 1;
-        buffEnd = min(NT, ops.timepoints);
+        buffEnd = min(NT, ops.sampsToRead);
         buff = h5read(ops.fbinary, '/sig', [offset NchanTOT], [buffEnd NchanTOT]);
     
         buff = buff'; % Transpose it so that it is channels x time and works for the rest of the script
